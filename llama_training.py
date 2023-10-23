8cf8b83817cb269a215e468219304d408a307bbf
Allan Lin <allanlin@666666@gmail.com>
2023-10-23 22:23:50 +0000

    .

--- llama_training.py
+++ llama_training.py
@@ -20,30 +20,36 @@ dataset = [
     torch.randint(0, config.vocab_size, (batch_size, seq_len), device='cuda', dtype=torch.int64) for i in range(samples)
 ]
 
-forward = []
-backward = []
-grad_step = []
-for x in tqdm(dataset):
-    torch.cuda.synchronize()
+
+forward = [torch.cuda.Event(enable_timing=True) for _ in range(len(dataset))]
+backward = [torch.cuda.Event(enable_timing=True) for _ in range(len(dataset))]
+grad_step1 = [torch.cuda.Event(enable_timing=True) for _ in range(len(dataset))]
+grad_step2 = [torch.cuda.Event(enable_timing=True) for _ in range(len(dataset))]
+torch.cuda.synchronize()
+for i, x in tqdm(enumerate(dataset)):
     t1 = time.time()
+    forward[i].record()
     with torch.autocast('cuda', torch.float16, enabled=True):
         y = model(x, labels=x)
-    torch.cuda.synchronize()
-    t2 = time.time()
+    backward[i].record()
     y.loss.backward()
-    torch.cuda.synchronize()
-    t3 = time.time()
+    grad_step1[i].record()
     optimizer.step()
     optimizer.zero_grad()
-    torch.cuda.synchronize()
-    t4 = time.time()
-    forward.append(t2 - t1)
-    backward.append(t3 - t2)
-    grad_step.append(t4 - t3)
+    grad_step2[i].record()
 
+torch.cuda.synchronize()
 quantile = lambda x: torch.quantile(torch.tensor(x), torch.tensor([0.5, 0.2, 0.8]))
 
+forward = [s.elapsed_time(e) for s, e in zip(forward, backward)]
+backward = [s.elapsed_time(e) for s, e in zip(backward, grad_step1)]
+grad_step = [s.elapsed_time(e) for s, e in zip(grad_step1, grad_step2)]
 print(f"Forward: {quantile(forward)}")
 print(f"Backward: {quantile(backward)}")
 print(f"Grad Step: {quantile(grad_step)}")
 
+
+# batch_size = 8, Mi210
+# Forward: tensor([0.0212, 0.0211, 0.0214])
+# Backward: tensor([0.0310, 0.0309, 0.0310])
+# Grad Step: tensor([0.0291, 0.0290, 0.0292])
