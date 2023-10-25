72fe90d2d55c9230f6098cd6fb30ff0a4291f79c
Allan Lin <allanlin@666666@gmail.com>
2023-10-25 14:16:13 -0400

    .

--- src/copy.cpp
+++ src/copy.cpp
@@ -20,6 +20,25 @@ void __global__ copy_kernel(const float* __restrict__ a, float* __restrict__ b, 
     }
 }
 
+void __global__ copy_kernel_pipeline(const float* __restrict__ a, float* __restrict__ b, const int n) {
+    const int stride = blockDim.x * gridDim.x;
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int i = 0;
+
+    float locals[2];
+    
+    if (tid < n) {
+        locals[0] = a[tid];
+    }
+
+    while (tid < n) {
+        b[tid] = locals[i];
+        tid += stride;
+        locals[(i + 1) % 2] = tid < n ? a[tid] : 0.0f;
+        i += 1;
+    }
+}
+
 void __global__ copy_kernelf4(const float4* __restrict__ a, float4* __restrict__ b, const int n) {
     const int stride = blockDim.x * gridDim.x;
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
@@ -44,6 +63,7 @@ int main(int argc, char** argv) {
 
     int grid_dim = (n + 1023) / 1024;
     hipLaunchKernelGGL(copy_kernel, dim3(grid_dim), dim3(1024), 0, 0, a, b, n);
+    hipLaunchKernelGGL(copy_kernel_pipeline, dim3(grid_dim), dim3(1024), 0, 0, a, b, n);
 
     if (n % 4 == 0) {
         grid_dim = (n / 4 + 1023) / 1024;
