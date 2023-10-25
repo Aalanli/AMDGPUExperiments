dd6e19ec6fd68bf01eac58748464c48d319c9f0e
Allan Lin <allanlin@666666@gmail.com>
2023-10-25 15:21:50 -0400

    .

--- src/copy.cpp
+++ src/copy.cpp
@@ -3,6 +3,8 @@
 #include <hip/amd_detail/amd_hip_vector_types.h>
 
 #include <cassert>
+#include <hip/hip_runtime_api.h>
+#include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,10 +16,10 @@ void __global__ copy_kernel(float* __restrict__ a, float* __restrict__ b, const 
     const int stride = blockDim.x * gridDim.x;
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
-    // while (tid < n) {
-        a[tid] += 1.0f;
-    //     tid += stride;
-    // }
+    while (tid < n) {
+        b[tid] = a[tid];
+        tid += stride;
+    }
 }
 
 void __global__ copy_kernel_pipeline(const float* __restrict__ a, float* __restrict__ b, const int n) {
@@ -50,6 +52,7 @@ void __global__ copy_kernelf4(const float4* __restrict__ a, float4* __restrict__
 }
 
 int main(int argc, char** argv) {
+    const int repeats = 1;
     assert(argc == 1);
     const int n = std::stoi(argv[1]);
     printf("copying %i floats\n", n);
@@ -61,12 +64,12 @@ int main(int argc, char** argv) {
     HIP_ASSERT(hipMalloc((void**)&a, n * sizeof(float)));
     HIP_ASSERT(hipMalloc((void**)&b, n * sizeof(float)));
 
-    int grid_dim = (n + 1023) / 1024;
+    int grid_dim = (n + 1023) / (1024 * repeats);
     hipLaunchKernelGGL(copy_kernel, dim3(grid_dim), dim3(1024), 0, 0, a, b, n);
     hipLaunchKernelGGL(copy_kernel_pipeline, dim3(grid_dim), dim3(1024), 0, 0, a, b, n);
 
     if (n % 4 == 0) {
-        grid_dim = (n / 4 + 1023) / 1024;
+        grid_dim = (n / 4 + 1023) / (1024 * repeats);
         hipLaunchKernelGGL(copy_kernelf4, dim3(grid_dim), dim3(1024), 0, 0, (float4*) a, (float4*) b, n / 4);
     }
 
